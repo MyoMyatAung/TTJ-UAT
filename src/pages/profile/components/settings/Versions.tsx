879acf403b6a24f0b57189db54016f36622ce6da
hlaing-dev <hlaingmin1235145@gmail.com>
2025-05-20 22:28:23 +0700

    update

--- src/pages/profile/components/settings/Versions.tsx
+++ src/pages/profile/components/settings/Versions.tsx
@@ -22,7 +22,7 @@ const Versions = ({ darkmode }: any) => {
                 darkmode ? " text-white" : "text-black"
               } versions-text`}
             >
-              V 1.1.5
+              V 1.1.6
             </h1>
           </div>
         </div>
