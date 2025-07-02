0214c4764fabb7482acc9c8f7f2e6aa5ee2c492b
ryanreynold21 <aunkyawin2035@gmail.com>
2025-07-02 19:09:49 +0700

    fix:route

--- src/pages/Point/Header.tsx
+++ src/pages/Point/Header.tsx
@@ -20,20 +20,23 @@ const Header: React.FC<HeaderProps> = ({}) => {
     }
   };
 
+  const goPOintmall = () => {
+    dispatch(setPointMall("/point_info"));
+    navigate("/point_mall");
+  };
+
   return (
     <div className=" flex justify-between items-center px-[20px] py-[10px]">
       <div onClick={routerLink}>
         <img src={back} className=" p-[20px" alt="" />
       </div>
       <h1 className=" text-white text-[18px] pl-[16px] font-[600]">我的积分</h1>
-      <a
-        onClick={() => dispatch(setPointMall("/point_info"))}
-        // target="_blink"
-        href="/point_mall"
+      <div
+       onClick={goPOintmall}
         className=" py-[8px] px-[10px mt-[5px"
       >
         <span className=" text-white text-[14px] font-[500]">积分商城</span>
-      </a>
+      </div>
     </div>
   );
 };
