f2985495e4764fe0654254fd5efada541f8236f9
MyoMyatAung <myatmyo299@gmail.com>
2025-10-22 15:02:16 +0700

    Chinese translation and set countdown zero

--- src/pages/social/components/WebViewPost.tsx
+++ src/pages/social/components/WebViewPost.tsx
@@ -24,7 +24,7 @@ const WebViewPost: React.FC<Props> = ({
   };
   return (
     <div className="relative">
-      {!post.unlock_post && <LockPost unlock={handleNavigate} unlockText="Click To View Details" />}
+      {!post.unlock_post && <LockPost unlock={handleNavigate} unlockText="点击查看详情" />}
       {post.files.map((file: any, index: any) => (
         <div
           key={index}
