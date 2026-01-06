e05e93927e88a93c6873e005c4cdf922739313c0
MyoMyatAung <myatmyo299@gmail.com>
2026-01-06 13:17:00 +0700

    Update FooterNav component to use safe-area-inset-bottom for improved padding handling

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -257,7 +257,7 @@ const Footer: FC = () => {
   return (
     <footer
       className={`bg-white dark:bg-[#1f1f21] fixed  transition-all duration-300 w-full shadow-lg z-[200] bottom-0`}
-      style={{ paddingBottom: 'env(safe-area-inset-bottom, 0)' }}
+      style={{ paddingBottom: 'env(safe-area-inset-bottom)' }}
     >
       <div
         className={`flex pt-4 justify-around items-center py-2  ${
